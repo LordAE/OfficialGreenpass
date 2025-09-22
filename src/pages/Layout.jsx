c174bc6b7d91445aa111809eb5235b4c7ab2b169
LordAE <32814844+LordAE@users.noreply.github.com>
2025-09-22 14:22:02 -0400

    Update Layout.jsx V2

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1088,7 +1088,7 @@ export default function Layout() {
   // Brand visuals
   const getLogoUrl = () =>
     brandSettings?.logo_url ||
-    "https://firebasestorage.googleapis.com/v0/b/greenpass-dc92d.firebasestorage.app/o/rawdatas%2FGreenPass%20Official.png?alt=media&token=809da08b-22f6-4049-bbbf-9b82342630e8";
+    "https://firebasestorage.googleapis.com/v0/b/greenpass-dc92d.firebasestorage.app/o/rawdatas%2FGreenPass%20Superapp.png?alt=media&token=987ad375-1aeb-4e1f-af08-7d89eb0ee2d8";
   const getCompanyName = () => brandSettings?.company_name || "GreenPass";
 
   if (loading) {
