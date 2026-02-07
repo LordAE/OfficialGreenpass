6d5f7e6cad37f8f5cbaf40954cd76c05198536df
Rancis09 <ryanontal@gmail.com>
2026-02-07 19:17:59 +0800

    Revert "auth"
    This reverts commit 0058608571c6b846247c31bc239f3ddb3726e2c3.

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ import StudyUnitedStates from "@/pages/countries/StudyUnitedStates";
 import Messages from "@/pages/Messages";
 import AdminSubscription from "./pages/AdminSubscription";
 import EventDetailsPage from "./pages/EventDetails";
-import AuthCallback from "@/pages/AuthCallBack";
+
 
 // --- Safe import of createPageUrl (with fallback if not exported) ---
 import * as Utils from "@/utils";
@@ -76,7 +76,6 @@ export default function App() {
         <Route index element={<Welcome />} />
         <Route path="welcome" element={<Welcome />} />
         <Route path="messages" element={<Messages />} />
-        <Route path="auth/callback" element={<AuthCallback />} />
 
         {/* Public site */}
         <Route path="directory" element={<Directory />} />
